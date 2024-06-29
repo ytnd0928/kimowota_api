d2127816abadeb9a615dba1e02d2d8c251a8b469
ytnd0928 <yetianxiongfei@gmail.com>
2024-06-29 17:17:44 +0900

    [add]セッティング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    User.all
+  end
+  def create
+    User.create(user_params) 
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :name, :password_hash, :icon_image)
+  end
 end
--- app/models/user_idol.rb
+++ app/models/user_idol.rb
@@ -1,2 +1,4 @@
 class UserIdol < ApplicationRecord
+  belongs_to :user
+  belongs_to :idol
 end
